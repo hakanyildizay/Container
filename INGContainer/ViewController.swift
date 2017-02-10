8750159d171122869bdf23be2c20eb6c15b53764
Hakan Yildizay [Mobil Yazilim  Servisi] <Hakan.Yildizay@ingbank.com.tr>
2017-02-10 17:21:50 +0300

    Initial Commit

---
+++ INGContainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  INGContainer
+//
+//  Created by Hakan Yildizay [Mobil Yazilim  Servisi] on 10/02/17.
+//  Copyright © 2017 ING BANK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
